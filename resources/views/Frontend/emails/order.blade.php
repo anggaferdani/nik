0132369bfa52d6d0243b5fe3b525b416c40e47c7
anggaferdani <eanggawahyu@gmail.com>
2024-08-28 10:37:50 +0700

    email

--- resources/views/Frontend/emails/order.blade.php
+++ resources/views/Frontend/emails/order.blade.php
@@ -4,54 +4,177 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Email Template</title>
+  <title></title>
+  <style>
+    body,
+    table,
+    td,
+    a {
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+    table,
+    td {
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+    img {
+      -ms-interpolation-mode: bicubic;
+    }
+    img {
+      border: 0;
+      height: auto;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+    }
+    table {
+      border-collapse: collapse !important;
+    }
+    body {
+      height: 100% !important;
+      margin: 0 !important;
+      padding: 0 !important;
+      width: 100% !important;
+    }
+    a[x-apple-data-detectors] {
+      color: inherit !important;
+      text-decoration: none !important;
+      font-size: inherit !important;
+      font-family: inherit !important;
+      font-weight: inherit !important;
+      line-height: inherit !important;
+    }
+    div[style*="margin: 16px 0;"] {
+      margin: 0 !important;
+    }
+  </style>
 </head>
-<body style="background-color: #f7f5fa; margin: 0; padding: 0; width: 100%;">
-
+<body style="background-color: #f7f5fa; margin: 0 !important; padding: 0 !important;">
   <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <tr>
-      <td bgcolor="#426899" align="center" style="padding: 40px 10px;">
-        <img src="{{ $message->embed('Images/logonik-hd.png') }}" alt="Logo" style="display: block; border: 0; outline: none; text-decoration: none; height: auto; line-height: 100%; max-width: 100%;">
+      <td bgcolor="#426899" align="center">
+        <table border="0" cellpadding="0" cellspacing="0" width="480">
+          <tr>
+            <td align="center" valign="top" style="padding: 40px 10px 40px 10px;">
+              <img src="{{ $message->embed('Images/logonik-hd.png') }}" alt="">
+            </td>
+          </tr>
+        </table>
       </td>
     </tr>
-
     <tr>
-      <td bgcolor="#f4f4f4" align="center" style="padding: 10px;">
-        <table border="0" cellpadding="0" cellspacing="0" width="480" style="width: 100%; max-width: 480px;">
+      <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="480">
           <tr>
-            <td bgcolor="#ffffff" align="left" style="padding: 20px; font-family: Arial, sans-serif; font-size: 14px; line-height: 20px; color: #333333;">
-              <p style="margin: 0 0 10px 0;"><strong>ID:</strong> {{ $id }}</p>
-              <p style="margin: 0 0 10px 0;"><strong>Nama:</strong> {{ $nama }}</p>
-              <p style="margin: 0 0 10px 0;"><strong>Email:</strong> {{ $email }}</p>
-              <p style="margin: 0 0 10px 0;"><strong>No. Telp:</strong> {{ $no_telp }}</p>
-              <p style="margin: 0 0 10px 0;"><strong>Alamat:</strong> {{ $alamat }}</p>
-
-              <table width="100%" border="1" cellspacing="0" cellpadding="10" style="border-collapse: collapse; border: 1px solid #dddddd;">
+            <td bgcolor="#ffffff" align="left">
+              <table width="100%" border="0" cellspacing="0" cellpadding="0">
                 <tr>
-                  <th align="left" style="font-family: Arial, sans-serif; font-size: 13px; font-weight: normal; border: 1px solid #dddddd;">Nama Barang</th>
-                  <th align="left" style="font-family: Arial, sans-serif; font-size: 13px; font-weight: normal; border: 1px solid #dddddd;">Jumlah</th>
-                  <th align="left" style="font-family: Arial, sans-serif; font-size: 13px; font-weight: normal; border: 1px solid #dddddd;">Total Harga</th>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    ID</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    {{ $id }}</td>
                 </tr>
-                @php $grandTotal = 0; @endphp
-                @foreach($orderItems as $item)
-                @php
-                  $totalPrice = $item->qty * $item->produk->harga;
-                  $grandTotal += $totalPrice;
-                @endphp
                 <tr>
-                  <td style="font-family: Arial, sans-serif; font-size: 13px; border: 1px solid #dddddd;">{{ $item->produk->nama }}</td>
-                  <td style="font-family: Arial, sans-serif; font-size: 13px; border: 1px solid #dddddd;">{{ $item->qty }}</td>
-                  <td style="font-family: Arial, sans-serif; font-size: 13px; border: 1px solid #dddddd;">Rp. {{ number_format($totalPrice, 2) }}</td>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    Nama</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    {{ $nama }}</td>
                 </tr>
-                @endforeach
                 <tr>
-                  <td colspan="2" style="text-align: right; font-family: Arial, sans-serif; font-size: 13px; border: 1px solid #dddddd;"><strong>Total Harga:</strong></td>
-                  <td style="font-family: Arial, sans-serif; font-size: 13px; border: 1px solid #dddddd;">Rp. {{ number_format($grandTotal, 2) }}</td>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    Email</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    {{ $email }}</td>
+                </tr>
+                <tr>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    No. Telp</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    {{ $no_telp }}</td>
+                </tr>
+                <tr>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    Alamat</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    {{ $alamat }}</td>
+                </tr>
+                <tr>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:30px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    Order Item</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:30px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    <table width="100%" border="1" cellspacing="0" cellpadding="10"
+                      style="border-collapse: collapse; border: 1px solid #dddddd;">
+                      <tr>
+                        <th align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          Nama Barang
+                        </th>
+                        <th align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          Jumlah
+                        </th>
+                        <th align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          Total Harga
+                        </th>
+                      </tr>
+                      @php
+                        $grandTotal = 0;
+                      @endphp
+                      @foreach($orderItems as $item)
+                      @php
+                        $totalPrice = $item->qty * $item->produk->harga;
+                        $grandTotal += $totalPrice;
+                      @endphp
+                      <tr>
+                        <td align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          {{ $item->produk->nama }}
+                        </td>
+                        <td align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          {{ $item->qty }}
+                        </td>
+                        <td align="left"
+                          style="padding-bottom:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          Rp. {{ number_format($totalPrice, 2) }}
+                        </td>
+                      </tr>
+                      @endforeach
+                      <tr>
+                        <td colspan="2"
+                          style="padding-top:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; text-align: right; border: 1px solid #dddddd;">
+                          <strong>Total Harga:</strong>
+                        </td>
+                        <td align="left"
+                          style="padding-top:10px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px; border: 1px solid #dddddd;">
+                          Rp. {{ number_format($grandTotal, 2) }}
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <th align="left" valign="top"
+                    style="padding-left:15px; padding-right:15px;padding-bottom:0px; font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;">
+                    Bukti Bayar</th>
+                  <td align="left" valign="top"
+                    style="padding-left:15px;padding-right:30px; padding-bottom:0px;font-family: Helvetica, Arial, sans-serif; font-size: 13px; font-weight: 400; line-height: 25px;"><img src="{{ $message->embed($buktiBayar) }}" alt=""></td>
                 </tr>
               </table>
-
-              <p style="margin: 20px 0 0 0;"><strong>Bukti Bayar:</strong></p>
-              <img src="{{ $message->embed($buktiBayar) }}" alt="Bukti Bayar" style="display: block; border: 0; outline: none; text-decoration: none; height: auto; line-height: 100%; max-width: 100%; margin-top: 10px;">
             </td>
           </tr>
         </table>
